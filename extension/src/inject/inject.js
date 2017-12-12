7b1bb81459e6fabbc7d9ab0c49b61c42ae60d2c9
Alejandro Pérez Martín <aleperez92@gmail.com>
2017-12-12 13:40:12 +0100

    Add downloading status to button. Move content to extension directory.

--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -3,8 +3,12 @@
 
 const tokenName = 'gh_personal_token';
 
-const headers = new Headers();
-headers.append('Accept', 'application/vnd.github.v3.text-match+json');
+const textMatchHeaders = new Headers();
+textMatchHeaders.append('Accept', 'application/vnd.github.v3.text-match+json');
+
+const downloadButton = document.createElement('button');
+
+const downloadText = chrome.i18n.getMessage('downloadBtn') || 'Download';
 
 /**
  * Adds a Download button to GitHub's search results page
@@ -13,16 +17,11 @@ headers.append('Accept', 'application/vnd.github.v3.text-match+json');
 function addDownloadButton() {
   const buttonsDiv = document.querySelector('.codesearch-results .select-menu');
 
-  const downloadButton = document.createElement('button');
+  if (!buttonsDiv) return;
+
   downloadButton.classList.add('btn', 'btn-sm');
   downloadButton.addEventListener('click', onDownload);
-
-  const downloadText = chrome.i18n.getMessage('downloadBtn') || 'Download';
-
-  downloadButton.innerHTML = `<div class="d-flex flex-space-between flex-items-center">${downloadText}
-    <svg version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="14" height="14" style="margin-left:5px;"><g fill="none" fill-rule="evenodd"><g fill="#000"><path d="m4 6h3v-6h2v6h3l-4 4-4-4zm11-4h-4v1h4v8h-14v-8h4v-1h-4c-0.55 0-1 0.45-1 1v9c0 0.55 0.45 1 1 1h5.34c-0.25 0.61-0.86 1.39-2.34 2h8c-1.48-0.61-2.09-1.39-2.34-2h5.34c0.55 0 1-0.45 1-1v-9c0-0.55-0.45-1-1-1z"/></g></g></svg>
-  </div>`;
-
+  downloadButton.innerHTML = `<div class="d-flex flex-space-between flex-items-center"><span>${downloadText}</span><svg version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="14" height="14" style="margin-left:5px;"><g fill="none" fill-rule="evenodd"><g fill="#000"><path d="m4 6h3v-6h2v6h3l-4 4-4-4zm11-4h-4v1h4v8h-14v-8h4v-1h-4c-0.55 0-1 0.45-1 1v9c0 0.55 0.45 1 1 1h5.34c-0.25 0.61-0.86 1.39-2.34 2h8c-1.48-0.61-2.09-1.39-2.34-2h5.34c0.55 0 1-0.45 1-1v-9c0-0.55-0.45-1-1-1z"/></g></g></svg></div>`;
   buttonsDiv.appendChild(downloadButton);
 }
 
@@ -35,28 +34,33 @@ function onDownload(e) {
   chrome.storage.sync.get({
     [tokenName]: ''
   }, item => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const searchQuery = encodeURI(urlParams.get('q'));
-
-    search(searchQuery, item[tokenName]);
+    downloadButton.querySelector('span').textContent = chrome.i18n.getMessage('downloadingStatus') || 'Downloading...';
+    downloadButton.disabled = true;
+    search(item[tokenName]);
   });
 }
 
 /**
- * Searches though code via GitHub API
+ * Searches through code via GitHub API
  *
- * @param {string} [params='']
  * @param {string} [token='']
  * @param {number} [page=1]
  * @param {number} [resultsCount=0]
  * @param {Array} [items=[]]
  * @returns
  */
-async function search(params = '', token = '', page = 1, resultsCount = 0, items = []) {
+async function search(token = '', page = 1, resultsCount = 0, items = []) {
+  const apiUrl = new URL('https://api.github.com/search/code');
+  const queryParams = new URLSearchParams(window.location.search).get('q') || '';
+
+  apiUrl.searchParams.set('q', queryParams);
+  apiUrl.searchParams.set('page', page);
+  apiUrl.searchParams.set('access_token', token);
+
   try {
-    const response = await fetch(`https://api.github.com/search/code?q=${params}&access_token=${token}&page=${page}`, {
+    const response = await fetch(apiUrl, {
       method: 'GET',
-      headers,
+      headers: textMatchHeaders,
       cache: 'default'
     });
     const data = await response.json();
@@ -66,7 +70,7 @@ async function search(params = '', token = '', page = 1, resultsCount = 0, items
 
     // Iterate through pages
     if (resultsCount < data.total_count) {
-      search(params, token, page + 1, resultsCount, items);
+      search(token, page + 1, resultsCount, items);
     } else { // No more pages, export file
       generateCsvFile(items);
     }
@@ -86,7 +90,7 @@ function generateCsvFile(data) {
 
     data.forEach(item => {
       const matched = item.text_matches.map(match => match.fragment);
-      csvContent += `${item.repository.owner.login},${item.repository.html_url},${escape(matched)}\n`;
+      csvContent += `${item.repository.owner.login},${encodeURI(item.repository.html_url)},${escape(matched)}\n`;
     });
 
     const blob = new Blob([csvContent], { type: 'text/csv;charset=base64' });
@@ -96,6 +100,10 @@ function generateCsvFile(data) {
     downloadLink.href = window.URL.createObjectURL(blob);
     downloadLink.download = fileName;
     document.body.appendChild(downloadLink);
+
+    downloadButton.disabled = false;
+    downloadButton.querySelector('span').textContent = downloadText;
+
     downloadLink.click();
     document.body.removeChild(downloadLink);
   } else {
@@ -106,8 +114,4 @@ function generateCsvFile(data) {
 /**
  * Called on app init
  */
-function init() {
-  addDownloadButton();
-}
-
-init();
+window.onload = addDownloadButton;
