d995cf75d341c74dbdac46ba39dd3df48350f3e5
Alejandro Pérez Martín <aleperez92@gmail.com>
2018-12-10 19:05:10 +0100

    Show no token set message and fix lint errors

--- extension/src/inject/inject.js
+++ extension/src/inject/inject.js
@@ -1,43 +1,77 @@
 /* eslint-disable no-alert */
-/* global chrome */
+const browser = window.browser || window.chrome;
 
-const tokenName = 'gh_personal_token';
+const tokenName = 'gscsv_personal_token';
 
 const textMatchHeaders = new Headers();
 textMatchHeaders.append('Accept', 'application/vnd.github.v3.text-match+json');
 
 const downloadButton = document.createElement('button');
-
-const downloadText = chrome.i18n.getMessage('downloadBtn') || 'Download';
+const downloadText = browser.i18n.getMessage('downloadBtn') || 'Download';
+const noTokenText = browser.i18n.getMessage('noTokenBtn') || 'Set a valid token in config';
 
 /**
- * Adds a Download button to GitHub's search results page
- * to export results to CSV
+ * Returns extension saved config
+ *
+ * @param {function} [callback=(token) => {}] Callback function
  */
-function addDownloadButton() {
-  const buttonsDiv = document.querySelector('.codesearch-results .select-menu');
-
-  if (!buttonsDiv) return;
-
-  downloadButton.classList.add('btn', 'btn-sm');
-  downloadButton.addEventListener('click', onDownload);
-  downloadButton.innerHTML = `<div class="d-flex flex-space-between flex-items-center"><span>${downloadText}</span><svg version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="14" height="14" style="margin-left:5px;"><g fill="none" fill-rule="evenodd"><g fill="#000"><path d="m4 6h3v-6h2v6h3l-4 4-4-4zm11-4h-4v1h4v8h-14v-8h4v-1h-4c-0.55 0-1 0.45-1 1v9c0 0.55 0.45 1 1 1h5.34c-0.25 0.61-0.86 1.39-2.34 2h8c-1.48-0.61-2.09-1.39-2.34-2h5.34c0.55 0 1-0.45 1-1v-9c0-0.55-0.45-1-1-1z"/></g></g></svg></div>`;
-  buttonsDiv.appendChild(downloadButton);
+function getToken(callback = (token) => {}) {
+  browser.storage.sync.get({
+    [tokenName]: ''
+  }, config => callback(config[tokenName]));
 }
 
 /**
- * Download button event handler
- *
- * @param {Event} e Event
+ * Opens extension options page
  */
-function onDownload(e) {
-  chrome.storage.sync.get({
-    [tokenName]: ''
-  }, item => {
-    downloadButton.querySelector('span').textContent = chrome.i18n.getMessage('downloadingStatus') || 'Downloading...';
-    downloadButton.disabled = true;
-    search(item[tokenName]);
-  });
+function openOptionsPage() {
+  if (browser.runtime.openOptionsPage) {
+    browser.runtime.openOptionsPage();
+  } else {
+    window.open(browser.runtime.getURL('src/options/options.html'));
+  }
+}
+
+/**
+ * Creates a blob downloadable file from a string
+ *
+ * @param {String} [content=''] File content
+ * @param {string} [filename='download']
+ */
+function downloadCsvFile(content = '', filename = 'download') {
+  const blob = new Blob([content], { type: 'text/csv;charset=utf-8' });
+  const link = window.document.createElement('a');
+  const fileName = `${filename}_${new Date().getTime()}.csv`;
+
+  link.href = window.URL.createObjectURL(blob);
+  link.download = fileName;
+  document.body.appendChild(link);
+
+  downloadButton.disabled = false;
+  downloadButton.querySelector('span').textContent = downloadText;
+
+  link.click();
+  document.body.removeChild(link);
+}
+
+/**
+ * Creates a CSV from a Response object
+ *
+ * @param {Object} data
+ */
+function generateCsvFile(data) {
+  if (data) {
+    let csvContent = 'Organization,Repository,Repository URL,Result file URL,Results\n';
+
+    data.forEach(item => {
+      const matched = item.text_matches.map(match => match.fragment.replace(/(?:\r\n|\r|\n|,|\s{2,})/g, ' ').toString());
+      csvContent += `${item.repository.owner.login},${item.repository.name},${encodeURI(item.repository.html_url)},${encodeURI(item.html_url)},${matched}\n`;
+    });
+
+    downloadCsvFile(csvContent, 'search_results');
+  } else {
+    alert('No results found');
+  }
 }
 
 /**
@@ -80,59 +114,54 @@ async function search(token = '', page = 1, resultsCount = 0, items = []) {
     } else { // No more pages, export file
       generateCsvFile(items);
     }
-  } catch (e) {
-    return e.message;
+  } catch (event) {
+    return event.message;
   }
 }
 
 /**
- * Creates a blob downloadable file from a string
+ * Download button event handler
  *
- * @param {String} [content=''] File content
- * @param {string} [filename='download']
+ * @param {Event} event Event
  */
-function downloadCsvFile(content = '', filename = 'download') {
-  const blob = new Blob([content], { type: 'text/csv;charset=utf-8' });
-  const link = window.document.createElement('a');
-  const fileName = `${filename}_${new Date().getTime()}.csv`;
+function onDownload(event) {
+  getToken(token => {
+    downloadButton.querySelector('span').textContent = browser.i18n.getMessage('downloadingStatus') || 'Downloading...';
+    downloadButton.disabled = true;
 
-  link.href = window.URL.createObjectURL(blob);
-  link.download = fileName;
-  document.body.appendChild(link);
-
-  downloadButton.disabled = false;
-  downloadButton.querySelector('span').textContent = downloadText;
-
-  link.click();
-  document.body.removeChild(link);
+    search(token);
+  });
 }
 
 /**
- * Creates a CSV from a Response object
- *
- * @param {any} data
+ * Adds a Download button to GitHub's search results page
+ * to export results to CSV
  */
-function generateCsvFile(data) {
-  if (data) {
-    let csvContent = 'Organization,Repository,Repository URL,Result file URL,Results\n';
+function addDownloadButton() {
+  getToken(token => {
+    const buttonsDiv = document.querySelector('#code_search_results');
 
-    data.forEach(item => {
-      const matched = item.text_matches.map(match => match.fragment.replace(/(?:\r\n|\r|\n|,|\s{2,})/g, ' ').toString());
-      csvContent += `${item.repository.owner.login},${item.repository.name},${encodeURI(item.repository.html_url)},${encodeURI(item.html_url)},${matched}\n`;
-    });
+    if (!buttonsDiv) return;
 
-    downloadCsvFile(csvContent, 'search_results');
-  }
-  else {
-    alert('No results found');
-  }
+    downloadButton.classList.add('btn', 'btn-sm', 'mt-4');
+
+    if (token) {
+      downloadButton.addEventListener('click', onDownload);
+      downloadButton.innerHTML = `<div class="d-flex flex-space-between flex-items-center"><span>${downloadText}</span><svg version="1.1" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="14" height="14" style="margin-left:5px;"><g fill="none" fill-rule="evenodd"><g fill="#000"><path d="m4 6h3v-6h2v6h3l-4 4-4-4zm11-4h-4v1h4v8h-14v-8h4v-1h-4c-0.55 0-1 0.45-1 1v9c0 0.55 0.45 1 1 1h5.34c-0.25 0.61-0.86 1.39-2.34 2h8c-1.48-0.61-2.09-1.39-2.34-2h5.34c0.55 0 1-0.45 1-1v-9c0-0.55-0.45-1-1-1z"/></g></g></svg></div>`;
+    } else {
+      downloadButton.addEventListener('click', openOptionsPage);
+      downloadButton.innerHTML = `<div class="d-flex flex-space-between flex-items-center"><span>${noTokenText}</span></div>`;
+    }
+
+    buttonsDiv.insertAdjacentElement('afterbegin', downloadButton);
+  });
 }
 
 /**
  * Initialize library
  */
 function init() {
-  const observeTarget = document.querySelector('body')
+  const observeTarget = document.querySelector('body');
   const observer = new MutationObserver(addDownloadButton);
 
   observer.observe(observeTarget, { childList: true });
